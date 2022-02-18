50038352a4d0f9ad7cd7aa49fb058e22e837903d
emilyg0310 <egonzalez70453@ufide.ac.cr>
2022-02-17 21:02:21 -0600

    S4 DogList

---
+++ app/src/main/java/com/example/doglist/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.doglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query:String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreed("$query/images")
+            val puppies = call.body()
+            if(call.isSuccessful){
+                //mostrar
+            }else{
+                //error
+            }
+        }
+    }
+}
